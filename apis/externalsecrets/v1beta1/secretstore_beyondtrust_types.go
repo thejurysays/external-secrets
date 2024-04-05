a106fc67ee49967629e282d4c54fbce8640881fc
Derek Jury <djury@beyondtrust.com>
2024-04-05 15:33:22 -0300

    feat: pulling ma from password safe
    Signed-off-by: Derek Jury <djury@beyondtrust.com>

--- apis/externalsecrets/v1beta1/secretstore_beyondtrust_types.go
+++ apis/externalsecrets/v1beta1/secretstore_beyondtrust_types.go
@@ -1,7 +1,22 @@
+/*
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
 package v1beta1
 
 import esmeta "github.com/external-secrets/external-secrets/apis/meta/v1"
 
+// Auth contains a secretRef for credentials.
 type BeyondTrustProviderSecretRef struct {
 
 	// Value can be specified directly to set a value without using a secret.
@@ -13,6 +28,7 @@ type BeyondTrustProviderSecretRef struct {
 	SecretRef *esmeta.SecretKeySelector `json:"secretRef,omitempty"`
 }
 
+// Configures a store to sync secrets using BeyondTrust Password Safe.
 type BeyondtrustProvider struct {
 	APIURL         string                        `json:"apiurl"`
 	Clientid       *BeyondTrustProviderSecretRef `json:"clientid"`
